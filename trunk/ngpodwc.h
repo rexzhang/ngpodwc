cf8a96c86078038fa6261d5311d06be0d423f016
rex <rex@a3416881-170b-0410-86bd-8295dfb7604d>
2006-01-28 16:44:39 +0000

    增加调试函数 现在的问题是表打开的参数不够

--- trunk/ngpodwc.h
+++ trunk/ngpodwc.h
@@ -6,8 +6,9 @@ struct ngpodwcConfig
 
 struct PodPictrueInfo
 {
-    wxString Title;
-    wxString PodDate;
+    //wxString Title;
+    wxChar Title[80];
+    wxChar PodDate[20];
     wxString Where;
     wxString When;
     wxString Who;
@@ -17,4 +18,32 @@ struct PodPictrueInfo
 };
 
 
-void getPodInfo(wxString DatabasePath, wxString DatabaseName, PodPictrueInfo *pPictureInfo);
+bool getPodInfo(const wxString DatabasePath, const wxString DatabaseName, PodPictrueInfo *pPictureInfo);
+
+// ----------------------------------------------------------------------------
+// FUNCTION USED FOR HANDLING/DISPLAYING ERRORS
+// ----------------------------------------------------------------------------
+// Very generic error handling function.
+// If a connection to the database is passed in, then we retrieve all the
+// database errors for the connection and add them to the displayed message
+int HandleError(wxString errmsg, wxDb *pDb=NULL)
+{
+    // Retrieve all the error message for the errors that occurred
+    wxString allErrors;
+    if (!pDb == NULL)
+        // Get the database errors and append them to the error message
+        allErrors = wxDbLogExtendedErrorMsg(errmsg.c_str(), pDb, 0, 0);
+    else
+        allErrors = errmsg;
+
+    // Do whatever you wish with the error message here
+    // wxLogDebug() is called inside wxDbLogExtendedErrorMsg() so this
+    // console program will show the errors in the console window,
+    // but these lines will show the errors in RELEASE builds also
+    //wxFprintf(stderr, wxT("\n%s\n"), allErrors.c_str());
+    //fflush(stderr);
+
+    wxSafeShowMessage(errmsg,allErrors);
+
+    return 1;
+};
