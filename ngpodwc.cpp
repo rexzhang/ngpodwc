83db686bad41e309d12e48cfb9e4390c3db25b1f
Rex Zhang <rex.zhang@gmail.com>
2006-01-31 14:50:11 +0000

    使用时间界定数据表操作问题部分解决；猜测出一个ODBC-Access的DATE类返回值，与年月日的关系 下一步的解决图片读取的问题： 1.GD2只能输出黑白两色的BMP格式文件 2.wxImage无法读取和写入BMP格式图片（可能是配置问题） 3.开始尝试GraphicsMagick

--- ngpodwc.cpp
+++ ngpodwc.cpp
@@ -16,9 +16,17 @@
 //#include <ctime>
 #include <time.h>
 
+//#include <wx/rawbmp.h>
+
 #include "ngpodwc.h"
 
+//GD2 Lib Support
+#include "gd.h"
 
+// ----------------------------------------------------------------------------
+// FUNCTION USED FOR MAIN <input point>
+// ----------------------------------------------------------------------------
+// return ?? = true/Finish
 main()
 {
 
@@ -26,8 +34,8 @@ main()
     wxFileInputStream ConfigInStream(wxT("ngpodwc.ini"));
     if(!ConfigInStream.Ok())//检查配置文件是否存在
     {
-        wxString msgTitle("配置文件不存在",*wxConvCurrent);
-        wxString msgContext("找不到检查配置文件 ngpodwc.ini\n请运行 ngpodcc.exe 进行初始化操作！",*wxConvCurrent);
+        wxString msgTitle("配置文件不存在！",*wxConvCurrent);
+        wxString msgContext("找不到检查配置文件 ngpodwc.ini！\n请运行 ngpodcc.exe 进行初始化操作！",*wxConvCurrent);
         wxSafeShowMessage(msgTitle, msgContext);
         //wxT("ngpodwc.ini can NOT found!,please RUN ngpodcc.exe"));
 
@@ -37,23 +45,46 @@ main()
     //建立到配置文件的连接
     wxFileConfig *pFileConfig = new wxFileConfig(ConfigInStream, wxConvUTF8);
 
-    //config->Write(wxT("DatabasePath"),wxT("d:\\"));
-
-    //读取配置文件-〉内存
+    //读取配置文件->内存
     ngpodwcConfig config;
-    config.DatabasePath = pFileConfig->Read(wxT("DatabasePath"));
-    config.DatabaseName = pFileConfig->Read(wxT("DatabaseName"));
-    //wxSafeShowMessage(config.DatabasePath,config.DatabaseName);
+    config.PodBasePath = pFileConfig->Read(wxT("PodBasePath"));
+
+    config.PodDatabaseName = pFileConfig->Read(wxT("PodDatabaseName"));
+    config.PodPicturePath = pFileConfig->Read(wxT("PodPicturePath"));
+
+    pFileConfig->Read(wxT("PodYear"), &(config.PodYear));
+    pFileConfig->Read(wxT("PodMonth"), &(config.PodMonth));
+    pFileConfig->Read(wxT("PodDays"), &(config.PodDays));
+
+    config.ScreenPicturePath = pFileConfig->Read(wxT("ScreenPicturePath"));
+    config.ScreenPictureName = pFileConfig->Read(wxT("ScreenPictureName"));
+    pFileConfig->Read(wxT("ScreenWidth"), &(config.ScreenWidth));
+    pFileConfig->Read(wxT("ScreenHeight"), &(config.ScreenHeight));
+
+    //wxSafeShowMessage(config.PodBasePath,config.PodDatabaseName);
 
     //获取POD 图片描述信息以及图片文件名称
-    PodPictrueInfo pPictureInfo;
-    if(!getPodInfo(config.DatabasePath, config.DatabaseName, &pPictureInfo))
+    PodPictrueInfo pictureInfo;
+    if(!getPodInfo(&config, &pictureInfo))
     {
-        wxSafeShowMessage(wxT("Error"),wxT("Error"));
+        wxString msgTitle("获取POD 图片描述信息以及图片文件名称错误！",*wxConvCurrent);
+        wxString msgContext("获取POD 图片描述信息以及图片文件名称错误！\n请运行 ngpodcc.exe 进行初始化操作！",*wxConvCurrent);
+        wxSafeShowMessage(msgTitle, msgContext);
+        return 1;
     }
 
+    wxSafeShowMessage(config.PodBasePath + wxT("\\")
+                      + config.PodPicturePath + wxT("\\")
+                      + pictureInfo.PhotoName,
+                      config.ScreenPicturePath + wxT("\\") + config.ScreenPictureName);
 
-
+    if(!outputScreenPicture(&config, &pictureInfo))
+    {
+        wxString msgTitle("图片Create Error错误！",*wxConvCurrent);
+        wxString msgContext("图片Create Error错误！\n请....XXXX.......操作！",*wxConvCurrent);
+        wxSafeShowMessage(msgTitle, msgContext);
+        return 1;
+    }
 
     //!!!TEST END
     wxSafeShowMessage(wxT("TEST END"),wxT("TEST END"));
@@ -62,11 +93,80 @@ main()
     return 0;
 }
 
-bool getPodInfo(const wxString DatabasePath, const wxString DatabaseName, PodPictrueInfo *pPictureInfo)
-//return 1 = true
+// ----------------------------------------------------------------------------
+// FUNCTION USED FOR xxxxxxx
+// ----------------------------------------------------------------------------
+// return 1 = true/Finish
+bool outputScreenPicture(ngpodwcConfig *pConfig, PodPictrueInfo *pPodPictureInfo)
 {
-    //wxSafeShowMessage(wxT("aaa"),DatabasePath + DatabaseName);
 
+    wxImage *pPodImage;
+    //wxImage PodImage;
+    wxInitAllImageHandlers();
+
+    //wxImage PodImage(wxT("MM6664_421.jpg"), wxBITMAP_TYPE_JPEG);
+    wxImage PodImage(wxT("MM6664_421.png"), wxBITMAP_TYPE_PNG);
+
+    /*    if (!PodImage.LoadFile(pConfig->PodBasePath + wxT("\\") + pConfig->PodPicturePath
+                               + wxT("\\") + pPodPictureInfo->PhotoName, wxBITMAP_TYPE_JPEG))
+            //    if (!PodImage.LoadFile(pConfig->PodBasePath + wxT("\\") + pConfig->PodPicturePath
+            //                         + wxT("\\") + wxT("MM6664_421.bmp"), wxBITMAP_TYPE_BMP))
+        {
+            wxSafeShowMessage(wxT("Can't load JPG image"),wxT("Can't load JPG image"));
+            return 0;
+        }
+        */
+
+    wxBitmap PictureBitmap;//(&PodImage);
+    //wxImage ScreenImage = PictureBitmap.ConvertToImage();
+
+    /*
+        if(!PictureBitmap.LoadFile(pConfig->ScreenPicturePath + wxT("\\")
+                                   + wxT("MM6664_421.bmp"), wxBITMAP_TYPE_BMP))
+        {
+            wxSafeShowMessage(wxT("Can't load BMP image"),wxT("Can't load BMP image"));
+            return 0;
+        }
+    */
+
+    //16777216
+
+    // Make a palette
+    unsigned char* red = new unsigned char[256];
+    unsigned char* green = new unsigned char[256];
+    unsigned char* blue = new unsigned char[256];
+    for (size_t i = 0; i < 256; i ++)
+    {
+        red[i] = green[i] = blue[i] = i;
+    }
+    wxPalette palette(256, red, green, blue);
+    // Set the palette and the BMP depth
+    PodImage.SetPalette(palette);
+
+    PodImage.SetOption(wxIMAGE_OPTION_BMP_FORMAT, wxBMP_8BPP_PALETTE);
+
+    //PodImage.SetOption(wxIMAGE_OPTION_BMP_FORMAT, wxBMP_24BPP);
+
+    //    if(!PodImage.SaveFile(pConfig->ScreenPicturePath + wxT("\\") + wxT("screen.bmp"),
+    //                        wxBITMAP_TYPE_BMP))
+    if(!PodImage.SaveFile(pConfig->ScreenPicturePath + wxT("\\") + wxT("screen.png"),
+                          wxBITMAP_TYPE_PNG))
+        //    if(!PodImage.SaveFile(config.ScreenPicturePath + wxT("\\") + wxT("screen.jpg"),
+        //                        wxBITMAP_TYPE_JPEG))
+    {
+        wxSafeShowMessage(wxT("Can't save BMP image"),wxT("Can't save BMP image"));
+        return 0;
+    };
+
+    return 1;
+}
+
+// ----------------------------------------------------------------------------
+// FUNCTION USED FOR xxxxxxx
+// ----------------------------------------------------------------------------
+// return 1 = true/Finish
+bool getPodInfo(ngpodwcConfig *pConfig, PodPictrueInfo *pPodPictureInfo)
+{
     wxDbConnectInf  *DbConnectInf    = NULL;    // DB connection information
 
     wxDb            *PodDB              = NULL;    // Database connection
@@ -86,14 +186,14 @@ bool getPodInfo(const wxString DatabasePath, const wxString DatabaseName, PodPic
 
     //透过Driver的方式打开ODBC(正式的打开ODBC操作)
     //if(DB->Open(wxT("DRIVER=Microsoft Access Driver (*.mdb);DBQ=D:\\pod.mdb;UID=admin;"),DBfailOnDataTypeUnsupported))
-    if(!PodDB->Open(wxT("DRIVER=Microsoft Access Driver (*.mdb);DBQ=") + DatabasePath + DatabaseName + wxT(";UID=admin;"),
+    if(!PodDB->Open(wxT("DRIVER=Microsoft Access Driver (*.mdb);DBQ=") + pConfig->PodBasePath + wxT("\\") + pConfig->PodDatabaseName + wxT(";UID=admin;"),
                     DBfailOnDataTypeUnsupported))
     {
         if (PodDB->IsOpen())
         {
             // Connection is open, but the initialization of
             // datatypes and parameter settings failed
-            wxSafeShowMessage(DatabasePath +DatabaseName,
+            wxSafeShowMessage(pConfig->PodBasePath + pConfig->PodDatabaseName,
                               wxT("Connection is open, but the initialization of datatypes and parameter settings failed"));
             return 0;
         }
@@ -101,7 +201,7 @@ bool getPodInfo(const wxString DatabasePath, const wxString DatabaseName, PodPic
         {
             // Error opening datasource
             //return HandleError(wxT("DB ENV ERROR: Cannot allocate ODBC env handle"));
-            wxSafeShowMessage(DatabasePath + DatabaseName,
+            wxSafeShowMessage(pConfig->PodBasePath + pConfig->PodDatabaseName,
                               wxT("Error opening datasource"));
             return 0;
         }
@@ -114,81 +214,60 @@ bool getPodInfo(const wxString DatabasePath, const wxString DatabaseName, PodPic
     table = new wxDbTable(PodDB, wxT("POD"), numTableColumns, wxT(""), wxDB_QUERY_ONLY, wxT(""));
 
     //清空数据
-    wxStrcpy(pPictureInfo->Title, wxT(""));
-    wxStrcpy(pPictureInfo->PodDate, wxT(""));
-    wxStrcpy(pPictureInfo->Where, wxT(""));
-    wxStrcpy(pPictureInfo->When, wxT(""));
-    wxStrcpy(pPictureInfo->Who, wxT(""));
-    wxStrcpy(pPictureInfo->Disc, wxT(""));
-    wxStrcpy(pPictureInfo->Related, wxT(""));
-    wxStrcpy(pPictureInfo->PhotoName, wxT(""));
+    wxStrcpy(pPodPictureInfo->Title, wxT(""));
+    wxStrcpy(pPodPictureInfo->PodDate, wxT(""));
+    wxStrcpy(pPodPictureInfo->Where, wxT(""));
+    wxStrcpy(pPodPictureInfo->When, wxT(""));
+    wxStrcpy(pPodPictureInfo->Who, wxT(""));
+    wxStrcpy(pPodPictureInfo->Disc, wxT(""));
+    wxStrcpy(pPodPictureInfo->Related, wxT(""));
+    wxStrcpy(pPodPictureInfo->PhotoName, wxT(""));
 
     time_t PodTime_TT=0;
 
-    table->SetColDefs(0, wxT("Pod_Title"), DB_DATA_TYPE_VARCHAR, pPictureInfo->Title, SQL_C_WXCHAR, sizeof(pPictureInfo->Title), true, true);
-//    table->SetColDefs(1, wxT("Pod_Date"), DB_DATA_TYPE_DATE, pPictureInfo->PodDate, SQL_C_WXCHAR, sizeof(pPictureInfo->PodDate), true, true);
-    table->SetColDefs(1, wxT("Pod_Date"), DB_DATA_TYPE_DATE, &PodTime_TT, SQL_C_DATE, sizeof(PodTime_TT), true, true);
-    table->SetColDefs(2, wxT("Pod_Where"), DB_DATA_TYPE_VARCHAR, pPictureInfo->Where, SQL_C_WXCHAR, sizeof(pPictureInfo->Where), true, true);
-    table->SetColDefs(3, wxT("Pod_When"), DB_DATA_TYPE_VARCHAR, pPictureInfo->When, SQL_C_WXCHAR, sizeof(pPictureInfo->When), true, true);
-    table->SetColDefs(4, wxT("Pod_Who"), DB_DATA_TYPE_VARCHAR, pPictureInfo->Who, SQL_C_WXCHAR, sizeof(pPictureInfo->Who), true, true);
-    table->SetColDefs(5, wxT("Pod_Disc"), DB_DATA_TYPE_VARCHAR, pPictureInfo->Disc, SQL_C_WXCHAR, sizeof(pPictureInfo->Disc), true, true);
-    table->SetColDefs(6, wxT("Pod_Related"), DB_DATA_TYPE_VARCHAR, pPictureInfo->Related, SQL_C_WXCHAR, sizeof(pPictureInfo->Related), true, true);
-    table->SetColDefs(7, wxT("Pod_Photo"), DB_DATA_TYPE_VARCHAR, pPictureInfo->PhotoName, SQL_C_WXCHAR, sizeof(pPictureInfo->PhotoName), true, true);
+    table->SetColDefs(0, wxT("Pod_Title"), DB_DATA_TYPE_VARCHAR, pPodPictureInfo->Title, SQL_C_WXCHAR, sizeof(pPodPictureInfo->Title), true, true);
+    table->SetColDefs(1, wxT("Pod_Date"), DB_DATA_TYPE_DATE, pPodPictureInfo->PodDate, SQL_C_WXCHAR, sizeof(pPodPictureInfo->PodDate), true, true);
+    //    table->SetColDefs(1, wxT("Pod_Date"), DB_DATA_TYPE_DATE, &PodTime_TT, SQL_C_DATE, sizeof(PodTime_TT), true, true);
+    table->SetColDefs(2, wxT("Pod_Where"), DB_DATA_TYPE_VARCHAR, pPodPictureInfo->Where, SQL_C_WXCHAR, sizeof(pPodPictureInfo->Where), true, true);
+    table->SetColDefs(3, wxT("Pod_When"), DB_DATA_TYPE_VARCHAR, pPodPictureInfo->When, SQL_C_WXCHAR, sizeof(pPodPictureInfo->When), true, true);
+    table->SetColDefs(4, wxT("Pod_Who"), DB_DATA_TYPE_VARCHAR, pPodPictureInfo->Who, SQL_C_WXCHAR, sizeof(pPodPictureInfo->Who), true, true);
+    table->SetColDefs(5, wxT("Pod_Disc"), DB_DATA_TYPE_VARCHAR, pPodPictureInfo->Disc, SQL_C_WXCHAR, sizeof(pPodPictureInfo->Disc), true, true);
+    table->SetColDefs(6, wxT("Pod_Related"), DB_DATA_TYPE_VARCHAR, pPodPictureInfo->Related, SQL_C_WXCHAR, sizeof(pPodPictureInfo->Related), true, true);
+    table->SetColDefs(7, wxT("Pod_Photo"), DB_DATA_TYPE_VARCHAR, pPodPictureInfo->PhotoName, SQL_C_WXCHAR, sizeof(pPodPictureInfo->PhotoName), true, true);
 
     //打开表
     if (!table->Open())
     {
-        wxSafeShowMessage(DatabasePath + DatabaseName,
+        wxSafeShowMessage(pConfig->PodBasePath + pConfig->PodDatabaseName,
                           wxT("An error occurred opening (setting up) the table"));
     }
 
     //table->SetWhereClause(wxT("Pod_When = '1982'"));//!OK
 
-    /*    wxDateTime PodDate;// = "2005-3-3";
-        PodDate.SetYear(2005);
-        PodDate.SetMonth(3);
-        PodDate.SetDay(3);
-    */
-    wxDateSpan PodDate;//=(2005,3,,3);
-    PodDate.SetYears(2005);
-    PodDate.SetMonths(3);
-    PodDate.SetDays(3);
-
-    wxDateTime PodDateTime = wxDateTime::Now();
-
-    PodDateTime.ResetTime();
-    PodDateTime.SetYear(2005);
-    //PodDateTime.
-    //PodDateTime.
-
-    //Month PodMonth;
+    //生成ODBC-Access的Date数字格式
+    //用tm格式生成wxDateTime格式，然后使用GetTicks转换成time_t格式
+    //再除以(24*60*60) 加上25539值
+    tm PodTm;
+    wxDateTime PodDateTime;
     time_t PodTime_t;
-
-    PodTime_t = PodDateTime.GetTicks();
-
-    tm PodTm;// = (0,0,0,3,3,2005-1900);
     PodTm.tm_sec = 0;
     PodTm.tm_min = 0;
     PodTm.tm_hour = 0;
-    PodTm.tm_mday = 3;
-    PodTm.tm_mon = 3;
-    PodTm.tm_year = 2004-1900;
+    PodTm.tm_mday = pConfig->PodDays;
+    PodTm.tm_mon = pConfig->PodMonth;
+    //PodTm.tm_year = 2004-1900;
+    PodTm.tm_year = pConfig->PodYear - 1900;
     PodTm.tm_wday = 0;
     PodTm.tm_yday = 0;
     PodTm.tm_isdst = 0;
-
     PodDateTime = wxDateTime::wxDateTime(PodTm);
-
     PodTime_t = PodDateTime.GetTicks();
+    PodTime_t = PodTime_t/(24*60*60) + 25539;
 
-    //PodMonth =PodDate.GetMonth();
-    //PodDate.Set(3,wxDateSpan::Months(3),2005,0,0,0,0);
-    //PodDate.SetYear(2005);
-    //wxString msg;
-    //msg.Printf(wxT("&u"),PodDate.wxDateTime_t);
-    //wxSafeShowMessage(msg,msg);
-
-    table->SetWhereClause(wxT("Pod_Date <= 40000 AND Pod_Date >= 37999"));
+    //table->SetWhereClause(wxT("Pod_Date <= 40000 AND Pod_Date >= 37999"));
+    wxString msg;
+    msg.Printf(wxT("Pod_Date = %u"),PodTime_t);
+    table->SetWhereClause(msg);
     //table->SetWhereClause(wxT("Pod_Date = 2001/03/13"));
 
     //按照PodDate字段排序
@@ -213,17 +292,17 @@ bool getPodInfo(const wxString DatabasePath, const wxString DatabaseName, PodPic
         wxString         msg;                       // Used for display messages
         msg.Printf(wxT("Row #% lu --\nTitle : %s\nPodDate : %s\nWhere : %s\nWhen : %s\nWho : %s\nDisc : %s\nRelated : %s\nPhotoName :%s"),
                    table->GetRowNum(),
-                   pPictureInfo->Title,
-                   pPictureInfo->PodDate,
-                   pPictureInfo->Where,
-                   pPictureInfo->When,
-                   pPictureInfo->Who,
-                   pPictureInfo->Disc,
-                   pPictureInfo->Related,
-                   pPictureInfo->PhotoName
+                   pPodPictureInfo->Title,
+                   pPodPictureInfo->PodDate,
+                   pPodPictureInfo->Where,
+                   pPodPictureInfo->When,
+                   pPodPictureInfo->Who,
+                   pPodPictureInfo->Disc,
+                   pPodPictureInfo->Related,
+                   pPodPictureInfo->PhotoName
                   );
-        // Code to display 'msg' here
-        wxSafeShowMessage(wxT("Pod_wxDbTable Test"),msg);
+        //检查表操作/现实获取的POD信息
+        //wxSafeShowMessage(wxT("Pod_wxDbTable Test"),msg);
     }
 
     /*
@@ -232,19 +311,77 @@ bool getPodInfo(const wxString DatabasePath, const wxString DatabaseName, PodPic
             wxString         msg;                       // Used for display messages
             msg.Printf(wxT("Row #% lu --\nTitle : %s\nPodDate : %s\nWhere : %s\nWhen : %s\nWho : %s\nDisc : %s\nRelated : %s\nPhotoName :%s"),
                        table->GetRowNum(),
-                       pPictureInfo->Title,
-                       pPictureInfo->PodDate,
-                       pPictureInfo->Where,
-                       pPictureInfo->When,
-                       pPictureInfo->Who,
-                       pPictureInfo->Disc,
-                       pPictureInfo->Related,
-                       pPictureInfo->PhotoName
+                       pPodPictureInfo->Title,
+                       pPodPictureInfo->PodDate,
+                       pPodPictureInfo->Where,
+                       pPodPictureInfo->When,
+                       pPodPictureInfo->Who,
+                       pPodPictureInfo->Disc,
+                       pPodPictureInfo->Related,
+                       pPodPictureInfo->PhotoName
                       );
             // Code to display 'msg' here
             wxSafeShowMessage(wxT("table->GetNext Error!"),msg);
             return 0;
         }
     */
-    return 1;
+    // -----------------------------------------------------------------------
+    // TAKE CARE OF THE ODBC CLASS INSTANCES THAT WERE BEING USED
+    // -----------------------------------------------------------------------
+    // If the wxDbTable instance was successfully created
+    // then delete it as we are done with it now.
+    wxDELETE(table);
+
+    // Free the cached connection
+    // (meaning release it back in to the cache of datasource
+    // connections) for the next time a call to wxDbGetConnection()
+    // is made.
+    wxDbFreeConnection(PodDB);
+    PodDB = NULL;
+
+
+    // -----------------------------------------------------------------------
+    // CLEANUP BEFORE EXITING APP
+    // -----------------------------------------------------------------------
+    // The program is now ending, so we need to close
+    // any cached connections that are still being
+    // maintained.
+    wxDbCloseConnections();
+
+    // Release the environment handle that was created
+    // for use with the ODBC datasource connections
+    wxDELETE(DbConnectInf);
+
+
+
+    return 1;//获取成功
+}
+
+
+bool outputScreenPictureGD2(ngpodwcConfig *pConfig, PodPictrueInfo *pPodPictureInfo)
+//return 1 = true/Finish
+{
+    gdImagePtr im;
+
+    FILE *in;
+    in = fopen("MM6664_421.jpg", "rb");
+    im = gdImageCreateFromJpeg(in);
+    fclose(in);
+
+    int black, white;
+    FILE *out;
+    /* Allocate background */
+    white = gdImageColorAllocate(im, 255, 255, 255);
+    /* Allocate drawing color */
+    black = gdImageColorAllocate(im, 0, 0, 0);
+    /* Draw rectangle */
+    gdImageRectangle(im, 0, 0, 99, 99, black);
+    /* Open output file in binary mode */
+    out = fopen("screen.bmp", "wb");
+    /* Write WBMP, with black as foreground */
+    gdImageWBMP(im, black, out);
+    /* Close file */
+    fclose(out);
+    /* Destroy image */
+    gdImageDestroy(im);
 }
