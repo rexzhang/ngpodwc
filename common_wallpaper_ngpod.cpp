4cada983ec11dde5879bce7c4c5592817655aef1
Rex Zhang <rex.zhang@gmail.com>
2006-03-04 09:41:13 +0000

    使用common_wallpaper_ngpod.cpp/h和common_wallpaper_base.cpp/h(基类)替换common_ngpod.cpp/h功能

---
+++ common_wallpaper_ngpod.cpp
@@ -0,0 +1,12 @@
+#include "common_wallpaper_ngpod.h"
+
+WallpaperNGPOD::WallpaperNGPOD(ngpodwcConfig programConfig):WallpaperBase(programConfig)
+{
+    //ctor
+}
+
+WallpaperNGPOD::~WallpaperNGPOD()
+{
+    //dtor
+}
+
