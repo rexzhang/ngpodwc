a987cca00c415f78be3ad441db865cb41815d00e
Rex Zhang <rex.zhang@gmail.com>
2006-03-04 10:24:25 +0000

    使用common_wallpaper_ngpod.cpp/h和common_wallpaper_base.cpp/h(基类)替换common_ngpod.cpp/h功能

--- common_wallpaper_ngpod.cpp
+++ common_wallpaper_ngpod.cpp
@@ -10,3 +10,33 @@ WallpaperNGPOD::~WallpaperNGPOD()
     //dtor
 }
 
+bool WallpaperNGPOD::Init()
+{
+    //获取POD 图片描述信息以及图片文件名称
+    if(!podPictureInfo.GetInfo(config.PodBasePath + wxT("\\") + config.PodDatabaseName, config.PodDate))
+    {
+        wxString msgTitle("获取POD 图片描述信息以及图片文件名称错误！",*wxConvCurrent);
+        wxString msgContext("获取POD 图片描述信息以及图片文件名称错误！\n请运行 ngpodcc.exe 进行初始化操作！",*wxConvCurrent);
+        wxSafeShowMessage(msgTitle, msgContext);
+        return false;
+    }
+    //PodImage.SetOption(wxIMAGE_OPTION_BMP_FORMAT,wxBMP_8BPP_GREY);
+    //PodImage.SetOption(wxIMAGE_OPTION_BMP_FORMAT,wxBMP_24BPP);
+    /*
+    //Debug Info
+    wxSafeShowMessage(pConfig->PodBasePath + wxT("\\")
+                      + pConfig->PodPicturePath + wxT("\\")
+                      + podPictureInfo.PhotoName,
+                      pConfig->ScreenPicturePath + wxT("\\") + pConfig->ScreenPictureName);
+                      */
+
+    //获取原始图片
+    if (!Image.LoadFile(config.PodBasePath + wxT("\\") + config.PodPicturePath
+                           + wxT("\\") + podPictureInfo.PhotoName, wxBITMAP_TYPE_JPEG))
+    {
+        wxSafeShowMessage(wxT("Can't load JPG image"), podPictureInfo.PhotoName);
+        return false;
+    }
+
+    return true;
+}
